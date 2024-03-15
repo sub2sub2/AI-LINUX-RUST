1f75cf5a675c39f641d8cd73c314cf64de725cec
sub2sub2 <tales47@naver.com>
2024-03-15 14:02:41 +0000

    Fix for dokcer env

---
+++ mca/src/api/main.rs
@@ -0,0 +1,20 @@
+mod app_ex;
+mod app_connection;
+
+use std::net::SocketAddr;
+use tonic::transport::Server;
+// use appEx::AppEx;
+use crate::app_connection::Connectable;
+
+// tokio: rust async library
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let address:SocketAddr = "172.17.0.2:4433".parse().unwrap();
+    // Instantiate TestServer directly since it implements Connectable
+    // let test_server = AppEx {};
+
+    // Pass the builder and address to the connect method
+    app_ex::AppEx::connect(Server::builder(), address).await?;
+
+    Ok(())
+}
